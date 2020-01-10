27a9f2d542072d16270758a7618fa61c267e6615
Ralph <ralph.d.dmello@gmail.com>
2020-01-10 10:57:45 -0800

    Added more funtionality - reset, resize and color random is working, working on shadding now.

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,73 @@
-// Loop for inputing grid.
-for (let i = 0; i < 16; i++)
+let tipChange = 0;
+
+// creating first grid of 16 x 16
+createBox(16);
+
+function createBox(num)
 {
-    for (let j = 0; j < 16; j++)
+    for (let i = 0; i < num; i++)
     {
-        const container = document.querySelector('.container');
-        const div = document.createElement('div');
-        // div.innerHTML = '<div></div>';
-        div.classList.add('box');
-        container.appendChild(div);
+        for (let j = 0; j < num; j++)
+        {
+            const container = document.querySelector('.container');
+            const div = document.createElement('div');
+            div.classList.add('box');
+            div.style.height = (512/num) + 'px';
+            div.style.width = (512/num) + 'px';
+            container.appendChild(div);
+        }
     }
 }
-// listing to the mouseover and changing color to black
-let boxes = document.querySelectorAll('.box');
-boxes.forEach(boxes => boxes.addEventListener('mouseenter', function(event){
-    event.target.style.backgroundColor = 'black';
-}));
-
-
-// let reset = document.querySelector('.reset');
-// reset.addEventListener('click', function() {
-//     boxes.forEach(boxes => boxes.style.backgroundColor = 'lightgray')
-// });
-// console.log(event);
 
 let reset = document.querySelector('.reset');
 reset.addEventListener('click', resetColor);
 
+let colorChangeBtn = document.querySelector('.colorChange');
+colorChangeBtn.addEventListener('click', colorChange);
+
 function resetColor() {
+    const boxes = document.querySelectorAll(".box");
     boxes.forEach(boxes => boxes.style.backgroundColor ='lightgray');
+    const promptBoxes = prompt("Enter number of boxes: ");
+    const container = document.querySelector('.container');
+    container.innerHTML = "";
+    createBox(promptBoxes);
+    tipChange = 0;
+    mouseover();
 }
+
+function colorChange() {
+    tipChange = 1;
+    mouseover();
+}
+
+// color change function 
+function getRandomColor() {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+}
+
+// listing to the mouseover and changing color to black
+function mouseover() {
+    let box = document.querySelectorAll('.box');
+    if (tipChange === 0) {
+        box.forEach(box => box.addEventListener('mouseenter', blackOut));
+    }
+    else {
+        box.forEach(box => box.addEventListener('mouseenter', setRandomColor));
+    }
+}
+
+function blackOut(e) {
+    this.style.backgroundColor = 'black';
+}
+
+function setRandomColor(e) {
+    this.style.backgroundColor = getRandomColor();
+}
+
+mouseover();
